eac87194f75644532ecad888fac482dc8332fcc3
joao gabriel medeiros pereira <gah.mp1@gmail.com>
2025-02-28 00:41:10 -0300

    criacao dos Styles dos dividers

---
+++ Packages/Zenith/Sources/Zenith/BaseElements/Divider/DividerStyles.swift
@@ -0,0 +1,84 @@
+import SwiftUI
+import ZenithCoreInterface
+
+public protocol DividerStyle: ViewModifier, Identifiable where ID == String {
+}
+
+public extension Divider {
+    func dividerStyle<T: DividerStyle>(_ style: T) -> some View {
+        modifier(style)
+    }
+}
+
+public struct PrimaryDividerStyle: DividerStyle, @preconcurrency BaseThemeDependencies {
+    public let id = String(describing: Self.self)
+    
+    @Dependency(\.themeConfigurator) public var themeConfigurator
+    
+    public func body(content: Content) -> some View {
+        content.foregroundStyle(colors.textPrimary)
+    }
+}
+
+public extension DividerStyle where Self == PrimaryDividerStyle {
+    static var primary: Self { Self() }
+}
+
+public struct SecondaryDividerStyle: DividerStyle, @preconcurrency BaseThemeDependencies {
+    public let id = String(describing: Self.self)
+    
+    @Dependency(\.themeConfigurator) public var themeConfigurator
+    
+    public func body(content: Content) -> some View {
+        content.foregroundStyle(colors.textSecondary)
+    }
+}
+
+public extension DividerStyle where Self == SecondaryDividerStyle {
+    static var secondary: Self { Self() }
+}
+
+public struct TertiaryDividerStyle: DividerStyle, @preconcurrency BaseThemeDependencies {
+    public let id = String(describing: Self.self)
+    
+    @Dependency(\.themeConfigurator) public var themeConfigurator
+    
+    public func body(content: Content) -> some View {
+        content.foregroundStyle(colors.primary)
+    }
+}
+
+public extension DividerStyle where Self == TertiaryDividerStyle {
+    static var tertiary: Self { Self() }
+}
+
+public enum StyleStyleColor: CaseIterable, Identifiable {
+    case primary
+    case secondary
+    case tertiary
+    
+    public var id: Self { self }
+    
+    var color: ColorName {
+        switch self {
+        case .primary:
+            .textPrimary
+        case .secondary:
+            .textSecondary
+        case .tertiary:
+            .primary
+        }
+    }
+    
+    @MainActor
+    public func style() -> AnyViewModifier {
+        switch self {
+        case .primary:
+            return AnyViewModifier(PrimaryDividerStyle())
+        case .secondary:
+            return AnyViewModifier(SecondaryDividerStyle())
+        case .tertiary:
+            return AnyViewModifier(PrimaryDividerStyle())
+        }
+    }
+}
