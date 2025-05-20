3b41bf55740e080c009d04363db4c52b6f20b43b
joao gabriel medeiros pereira <gah.mp1@gmail.com>
2025-05-20 12:05:48 -0300

    ajuste no checkbox

--- Packages/Zenith/Sources/Zenith/BaseElements/Customs/CheckBox/CheckBox.swift
+++ Packages/Zenith/Sources/Zenith/BaseElements/Customs/CheckBox/CheckBox.swift
@@ -2,55 +2,6 @@ import Dependencies
 import SwiftUI
 import ZenithCoreInterface
 
-public struct CheckBoxItem: Identifiable {
-    let placeholder: String
-    public let id: String
-    
-    public init(placeholder: String, id: String) {
-        self.placeholder = placeholder
-        self.id = id
-    }
-}
-
-public struct CheckBoxBundle: View, @preconcurrency BaseThemeDependencies {
-    @Dependency(\.themeConfigurator) public var themeConfigurator
-    @Binding private var selectedItems: Set<String>
-    let items: [CheckBoxItem]
-    
-    public init(selectedItems: Binding<Set<String>>, items: [CheckBoxItem]) {
-        self._selectedItems = selectedItems
-        self.items = items
-    }
-    
-    public var body: some View {
-        HStack {
-            Spacer()
-            ForEach(items) { item in
-                VStack {
-                    if item.placeholder.isNotEmpty {
-                        Text(item.placeholder)
-                            .textStyle(.small(.contentA))
-                            .padding(spacings.extraSmall)
-                    }
-                    CheckBox(
-                        isSelected: Binding(
-                            get: { selectedItems.contains(item.id) },
-                            set: { isSelected in
-                                if isSelected {
-                                    selectedItems.insert(item.id)
-                                } else {
-                                    selectedItems.remove(item.id)
-                                }
-                            }
-                        )
-                    )
-                }
-                Spacer()
-            }
-        }
-    }
-}
-
 public struct CheckBox: View {
     @Environment(\.checkBoxStyle) private var style
     let text: String
